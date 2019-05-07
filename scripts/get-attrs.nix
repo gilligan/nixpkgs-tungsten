404b1a1f308fdcc4be2f58dbc5b9342d41006298
tobias pflug <tobias.pflug@gmail.com>
2019-05-07 14:04:39 +0200

    Rewrite get-attrs to use contrail-versions
    This changes `get-attrs.nix` such that it only relies on the version
    information obtained through `contrail-versions.nix`.

--- scripts/get-attrs.nix
+++ scripts/get-attrs.nix
@@ -1,18 +1,20 @@
 { pkgs ? import <nixpkgs> {} }: 
+
 let
+
   inherit (pkgs) lib;
-  contrailVersions = [ "contrail32" "contrail41" "contrail50" ];
+  versions = import ../contrail-versions.nix;
   jobset = import ../jobset.nix {};
+
+  contrailVersions = map (x: "contrail${lib.concatStrings (lib.splitString "." x)}") versions;
   prefixWith = prefix: xs: map (x: "${prefix}.${x}") xs;
-  general = lib.subtractLists ["contrail32" "contrail41" "contrail50"]  (lib.attrNames jobset);
-  contrail32 = prefixWith "contrail32" (lib.attrNames jobset."contrail32");
-  contrail41 = prefixWith "contrail41" (lib.attrNames jobset."contrail41");
-  contrail50 = prefixWith "contrail50" (lib.attrNames jobset."contrail50");
-  contrail32Tests = prefixWith "contrail32.test" (lib.attrNames jobset.contrail32.test);
-  contrail41Tests = prefixWith "contrail41.test" (lib.attrNames jobset.contrail41.test);
-  contrail50Tests = prefixWith "contrail50.test" (lib.attrNames jobset.contrail50.test);
+  general = lib.subtractLists contrailVersions  (lib.attrNames jobset);
+  allContrailAttrs = lib.concatMap (x: prefixWith x (lib.attrNames jobset."${x}")) contrailVersions;
+  allContrailTests = lib.concatMap (x: prefixWith "${x}.test" (lib.attrNames jobset."${x}".test)) contrailVersions;
+
 in
+
   {
-    all = general ++ contrail32 ++ contrail41 ++ contrail50;
-    tests = contrail32Tests ++ contrail41Tests ++ contrail50Tests;
+    all = general ++ allContrailAttrs;
+    tests = allContrailTests;
   }
